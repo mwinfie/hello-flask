e3f8f72e28b612b88e1905c5c2b5e3add4462f1f
Matthew Winfield <mwinfie@gmail.com>
2018-04-08 11:06:24 -0500

    Correcting hostname return statement

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>{}</h1>".socket.gethostname()
+    return "<h1 style='color:blue'>{}</h1>".format(socket.gethostname())
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
