a23c97e33165d9aa1f21ff192ff56d5ce65c8f0d
Matthew Winfield <mwinfie@gmail.com>
2018-04-08 10:54:24 -0500

    Changing app to return hostname to verify load balancing

--- myflaskapp.py
+++ myflaskapp.py
@@ -1,9 +1,10 @@
+import socket
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello World!</h1>"
+    return "<h1 style='color:blue'>{}</h1>".socket.gethostname()
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
